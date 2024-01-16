cb1d1f88e170275e06b377f74fb35d5124241449
DashaKol88 <dashytamakarenko88@gmail.com>
2024-01-16 17:22:01 +0200

    create new project, create models, and register

---
+++ taskapp/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=50)
+    project_description = models.TextField(blank=True)
+    deadline = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    STATUS_CHOICES = [('new', 'New'), ('in_progress', 'In progress'), ('completed', 'Completed')]
+    title = models.CharField(max_length=50)
+    task_description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
